f81085f8cc51bbe370e14e48540147ab6aab7879
PlenkinAv <54805570+PlenkinAv@users.noreply.github.com>
2019-09-23 02:10:15 +0300

    Update 326. Power of Three.cpp

--- 326. Power of Three.cpp
+++ 326. Power of Three.cpp
@@ -1,6 +1,4 @@
-/* 
-https://leetcode.com/problems/power-of-three/
-Given an integer, write a function to determine if it is a power of three. */
+//https://leetcode.com/problems/power-of-three/
 
 class Solution {
 public:
