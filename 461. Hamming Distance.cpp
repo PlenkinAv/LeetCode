95b1a0acdc0e266d40520a4d430b3cbcd3617ad2
PlenkinAv <54805570+PlenkinAv@users.noreply.github.com>
2019-09-23 02:11:36 +0300

    Update 461. Hamming Distance.cpp

--- 461. Hamming Distance.cpp
+++ 461. Hamming Distance.cpp
@@ -1,8 +1,4 @@
-/*
-https://leetcode.com/problems/hamming-distance/
-The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
-
-Given two integers x and y, calculate the Hamming distance. */
+//https://leetcode.com/problems/hamming-distance/
 
 class Solution {
 public:
