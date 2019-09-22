5400ac1da12a4b5ee39b802dd4843e330079e810
PlenkinAv <54805570+PlenkinAv@users.noreply.github.com>
2019-09-23 02:09:33 +0300

    Update 172. Factorial Trailing Zeroes.cpp

--- 172. Factorial Trailing Zeroes.cpp
+++ 172. Factorial Trailing Zeroes.cpp
@@ -1,6 +1,4 @@
-/* 
-https://leetcode.com/problems/factorial-trailing-zeroes/
-Given an integer n, return the number of trailing zeroes in n!. */
+//https://leetcode.com/problems/factorial-trailing-zeroes/
 
 class Solution {
 public:
