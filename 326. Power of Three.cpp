35cefbb06ccd07088ae4fddb7ced325f8a60b522
PlenkinAv <54805570+PlenkinAv@users.noreply.github.com>
2019-09-22 23:50:37 +0300

    Update and rename isPowerOfThree.cpp to 326. Power of Three.cpp

--- 326. Power of Three.cpp
+++ 326. Power of Three.cpp
@@ -1,4 +1,6 @@
-/* Given an integer, write a function to determine if it is a power of three. */
+/* 
+https://leetcode.com/problems/power-of-three/
+Given an integer, write a function to determine if it is a power of three. */
 
 class Solution {
 public:
